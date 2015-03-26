7b669af5f0823d25e8eca2c3489d68b850d1fdfd
Razvan Valentin Marinescu <mrazvan22@gmail.com>
2015-03-26 16:48:30 +0000

    tesed some params for reg_f3d

--- cw/checkNii.m
+++ cw/checkNii.m
@@ -2,24 +2,32 @@ function checkNii()
 
 addpath NIFTI_20110921
 
-img_seg = load_nii('longitudinal_images/AD_0_baseline_seg.nii');
+img_orig = load_nii('longitudinal_images/AD_0_baseline.nii');
 
-img_orig = load_nii('longitudinal_images/AD_0_baseline_brain.nii');
+img_seg_mine = load_nii('longitudinal_images/AD_0_baseline_seg.nii');
 
-diff_img = img_seg;
-diff_img.img = img_seg.img - img_orig.img;
+img_seg_orig = load_nii('longitudinal_images/AD_0_baseline_brain.nii');
+
+diff_img = img_seg_mine;
+diff_img.img = img_seg_mine.img - img_seg_orig.img;
 
 for i=0:10
+  
   figure(1)
-  dispNiiSlice(diff_img, 'z', 100 + 10*i);
-  title('diff img')
+  dispNiiSlice(img_orig, 'z', 100 + 10*i);
+  title('orig img')
   
   figure(2)
-  dispNiiSlice(img_seg, 'z', 100+10*i);
-  title('img seg')
+  dispNiiSlice(diff_img, 'z', 100 + 10*i);
+  title('diff seg img')
   
   figure(3)
-  dispNiiSlice(img_orig, 'z', 100+10*i);
-  title('img_orig')
+  dispNiiSlice(img_seg_mine, 'z', 100+10*i);
+  title('img my seg')
+  
+  figure(4)
+  dispNiiSlice(img_seg_orig, 'z', 100+10*i);
+  title('img seg orig')
 end
+
 end
--- cw/p1.py
+++ cw/p1.py
@@ -19,21 +19,36 @@ nonlin_cpp_filenames = [name.split(".")[0] + "_nonlin_cpp.nii" for name in long_
 segmented_filenames = [name.split(".")[0] + "_seg.nii" for name in long_filenames]
 labeled_temp_filenames = [name.split(".")[0] + "_brain.nii" for name in template_filenames]
 
-for i in [0]:#range(len(long_filenames)):
+aladin_options = "-speeeeed ";
+
+# max 4 levels, 300 iterations each
+f3d_options = "-ln 4 -maxit 200"; # I think this would be ideal
+# 2 lvl 200 it - 1m26sec
+# 3 lvl 300 it - 2m16sec
+# 4 lvl 300 it - 2m18sec
+# 4 lvl 200 it - 1m30sec
+
+exec_cmd = 0;
+
+#os.system("cd logitudinal_images/")
+for i in range(len(long_filenames)):
   
   t = 0;
-  aff_reg_cmd = 'reg_aladin -ref %s -flo %s -res %s -aff %s' % (long_filenames[i], template_filenames[t], aff_reg_filenames[i], aff_mat_filenames[i])
+  aff_reg_cmd = 'reg_aladin -ref %s -flo %s -res %s -aff %s %s' % (long_filenames[i], template_filenames[t], aff_reg_filenames[i], aff_mat_filenames[i], aladin_options)
   print aff_reg_cmd
-  os.system(aff_reg_cmd)
+  if exec_cmd:
+    os.system(aff_reg_cmd)
   
-  nonlin_reg_cmd = 'reg_f3d -ref %s -flo %s -res %s -aff %s -cpp %s' % (long_filenames[i], template_filenames[t], nonlin_reg_filenames[i], aff_mat_filenames[i], nonlin_cpp_filenames[i])
+  nonlin_reg_cmd = 'reg_f3d -ref %s -flo %s -res %s -aff %s -cpp %s %s' % (long_filenames[i], template_filenames[t], nonlin_reg_filenames[i], aff_mat_filenames[i], nonlin_cpp_filenames[i], f3d_options)
 
   print nonlin_reg_cmd
-  os.system(nonlin_reg_cmd)
+  if exec_cmd:
+    os.system(nonlin_reg_cmd)
 
   resample_cmd = 'reg_resample -ref %s -flo %s -res %s -trans %s -inter 0' % (long_filenames[i], labeled_temp_filenames[t], segmented_filenames[i], nonlin_cpp_filenames[i])
 
   print resample_cmd
-  os.system(resample_cmd)
+  if exec_cmd:
+    os.system(resample_cmd)
 
 
